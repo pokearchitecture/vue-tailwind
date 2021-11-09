2200ed1098f7b5546f802969407ef41133fb555f
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2021-11-08 21:15:18 -0600

    Display list of Pokemon names

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onBeforeMount, ref } from 'vue';
+import PokeAPI, { INamedApiResource, IPokemon } from 'pokeapi-typescript';
 
-const message = ref('hello')
+let pokemon = ref<Array<INamedApiResource<IPokemon>>>();
+
+onBeforeMount(async () => {
+  const response = await PokeAPI.Pokemon.list(151);
+  pokemon.value = response.results;
+});
 </script>
 
 <template>
-<div>{{ message }}</div>
+  <div class="pl-5">
+    <div class="flex justify-center">
+      <ul class="list-disc">
+        <li v-for="mon in pokemon">
+          {{ mon.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
