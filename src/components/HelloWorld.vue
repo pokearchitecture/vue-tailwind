f43ff5ab695bf0c8bcefd6b16ee676c50bcb7812
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2021-11-08 20:41:53 -0600

    Install Tailwind CSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
+  <p class="font-bold">
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
