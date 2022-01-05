b5a09c8b9ec247a504e3cad6eeccf7ea1b02077b
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2022-01-05 17:26:13 -0600

    Create <Spinner />

---
+++ src/components/spinner/Spinner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center justify-center h-full">
+    <div
+      class="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"
+    ></div>
+  </div>
+</template>
