83cea976ad22470a5b4d60df2ee332e4f662691f
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2022-01-05 19:41:14 -0600

    Restyle and restructure <Navbar />

---
+++ src/components/nav-bar/NavLink.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+const boxShadow = 'box-shadow: inset 0 0 3px #0d1117;';
+
+const props = defineProps<{
+  text: string;
+  to: string;
+}>();
+
+const { text, to } = toRefs(props);
+</script>
+
+<template>
+  <div
+    class="
+      py-2
+      w-full
+      text-left
+      pl-5
+      bg-darkgray-500
+      hover:bg-green-400
+      cursor-pointer
+    "
+    :style="boxShadow"
+  >
+    <router-link :to="to" class="w-full h-full inline-block">{{
+      text
+    }}</router-link>
+  </div>
+</template>
---
+++ src/components/nav-bar/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavLink from './NavLink.vue';
+</script>
+
+<template>
+  <div class="text-white text-2xl font-bold">
+    <NavLink style="margin-top: 0.19rem" to="kanto" text="Kanto" />
+    <NavLink to="johto" text="Johto" />
+    <NavLink to="hoenn" text="Hoenn" />
+    <NavLink to="sinnoh" text="Sinnoh" />
+    <NavLink to="unova" text="Unova" />
+    <NavLink to="kalos" text="Kalos" />
+    <NavLink to="alola" text="Alola" />
+    <NavLink to="galar" text="Galar" />
+    <NavLink to="all" text="All" />
+  </div>
+</template>
